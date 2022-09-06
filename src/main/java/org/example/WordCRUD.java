227c72273b130644cc06c80c12feedba8adcd842
hyunwookoo13 <kyo972@naver.com>
2022-09-06 13:57:37 +0900

    Second commit

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -26,7 +26,7 @@ public class WordCRUD implements ICRUD{
 
         System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();
-
+        System.out.print("\n");
 
 
         return new Word(0, level, word, meaning);
@@ -35,7 +35,7 @@ public class WordCRUD implements ICRUD{
     public void addWord() {
         Word one = (Word)add();
         list.add(one);
-        System.out.println("새 단어가 단어장에 추가되었습니다. ");
+        System.out.println("새 단어가 단어장에 추가되었습니다. \n");
     }
 
     @Override
