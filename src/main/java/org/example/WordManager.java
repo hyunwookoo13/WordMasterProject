53d6e1df14581d0f76eb83e0c66a5ec184e67101
hyunwookoo13 <kyo972@naver.com>
2022-09-11 14:00:54 +0900

    search word&level part's commit

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -12,7 +12,7 @@ public class WordManager {
     }
 
     public int selectMenu() {
-        System.out.println("*** 영단어 마스터 ***\n"
+        System.out.print("*** 영단어 마스터 ***\n"
                 +"******************\n"
                 +"1. 모든 단어 보기\n"
                 +"2. 수준별 단어 보기\n"
@@ -40,6 +40,12 @@ public class WordManager {
             }
             else if(menu==1) {
                 wordCRUD.listAll();
+            }
+            else if(menu==2) {
+                wordCRUD.searchLevel();
+            }
+            else if(menu==3) {
+                wordCRUD.searchWord();
             }
             else if(menu==5) { //update
                 wordCRUD.updateItem();
