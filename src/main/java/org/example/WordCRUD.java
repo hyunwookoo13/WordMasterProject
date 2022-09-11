786d2a8db1be5c14ddf703e1b74086556d83347e
hyunwookoo13 <kyo972@naver.com>
2022-09-11 13:27:56 +0900

    loadfile part's commit

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
     ArrayList<Word> list;
     Scanner s;
+    final String fname = "Dictionary.txt";
 	/*
 	 => 난이도(1,2,3) & 새 단어 입력: 1 driveway
 	  뜻 입력 : 차고 진입로
@@ -111,5 +116,31 @@ public class WordCRUD implements ICRUD{
         }else {
             System.out.println("취소되었습니다. ");
         }
+    }
+
+    public void loadFile(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count =0;
+            while(true){
+                line=br.readLine();
+                if(line==null) break;
+
+                String data[] = line.split("\\|"); // 백슬레시 2개를 넣어주어야 버티클바를 인식한다.
+                int level = Integer.parseInt(data[0]); //String을 int로 바꿔주는 parseint 사용
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0,level,word, meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("=> "+ count + "개 로딩 완료!!!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
